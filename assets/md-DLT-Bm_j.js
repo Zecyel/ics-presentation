86615dc070285eb811bbfb41fadad56d61897d60
Zecyel <zecyel@163.com>
2024-12-04 15:39:37 +0800

    doc: create

---
+++ assets/md-DLT-Bm_j.js
@@ -0,0 +1,34 @@
+import{_ as c}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-C7-jKLH6.js";import{o as d,c as u,k as a,e as s,l as r,m as p,A as n,q as m,s as f,B as i}from"./modules/vue-BRBJo7Aj.js";import{_ as g}from"./slidev/two-cols.vue_vue_type_script_setup_true_lang-D0zzpJGx.js";import{u as k,f as x}from"./slidev/context-D8KpIxWg.js";import"./modules/unplugin-icons-DCrcQoyj.js";import"./index-DwpsrwPE.js";import"./modules/shiki-BhTto0IE.js";const h={style:{"padding-right":"20px"}},_={style:{"padding-top":"85px"}},R={__name:"slides.md__slidev_9",setup(v){const{$slidev:b,$nav:$,$clicksContext:t,$clicks:j,$page:y,$renderContext:B,$frontmatter:o}=k();return t.setup(),(C,l)=>{const e=c;return d(),u(g,m(f(i(x)(i(o),8))),{right:a(P=>[s("div",_,[r(e,p({},{ranges:[]}),{default:a(()=>l[1]||(l[1]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"ld r1, [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"neg r1")]),n(`
+`),s("span",{class:"line"},[s("span",null,"mov r0, 0xc")]),n(`
+`),s("span",{class:"line"},[s("span",null,"add r1, r2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"jz abs [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"nop")]),n(`
+`),s("span",{class:"line"},[s("span",null,"jmp rel [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"out r2, [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"xor r0, r0")]),n(`
+`),s("span",{class:"line"},[s("span",null,"ld r1, [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"nand r1, r1")]),n(`
+`),s("span",{class:"line"},[s("span",null,"neg r1")]),n(`
+`),s("span",{class:"line"},[s("span",null,"str r1, [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"mov r0, 0x4")]),n(`
+`),s("span",{class:"line"},[s("span",null,"jmp rel [r0]")])])],-1)])),_:1},16)])]),default:a(()=>[l[2]||(l[2]=s("h1",null,"A sample code",-1)),l[3]||(l[3]=s("p",null,"A code to print all the primes.",-1)),s("div",h,[r(e,p({},{ranges:[]}),{default:a(()=>l[0]||(l[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"byte: 0x02")]),n(`
+`),s("span",{class:"line"},[s("span",null,"xor r2, r2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"mov r0, 0xa")]),n(`
+`),s("span",{class:"line"},[s("span",null,"xor r0, r0")]),n(`
+`),s("span",{class:"line"},[s("span",null,"ld r1, [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"xor r3, r3")]),n(`
+`),s("span",{class:"line"},[s("span",null,"add r3, r2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"neg r3")]),n(`
+`),s("span",{class:"line"},[s("span",null,"xor r0, r0")]),n(`
+`),s("span",{class:"line"},[s("span",null,"nand r0, r0")]),n(`
+`),s("span",{class:"line"},[s("span",null,"add r1, r3")]),n(`
+`),s("span",{class:"line"},[s("span",null,"jo abs [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"add r1, r2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"xor r0, r0")]),n(`
+`),s("span",{class:"line"},[s("span",null,"mov r3, 0x0")]),n(`
+`),s("span",{class:"line"},[s("span",null,"snd r1")]),n(`
+`),s("span",{class:"line"},[s("span",null,"jz abs [r0]")]),n(`
+`),s("span",{class:"line"},[s("span",null,"nand r2, r2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"neg r2")]),n(`
+`),s("span",{class:"line"},[s("span",null,"xor r0, r0")])])],-1)])),_:1},16)])]),_:1},16)}}};export{R as default};
